55faf21730038bd1ef9441611a51d34be4d08e52
Dominic Law <dominic.law79@gmail.com>
2021-07-06 23:57:51 +1000

    Initial project setup

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
+const dotenv = require('dotenv');
+const connectDB = require('./config/db')
+const coingecko = require("./routes/api/coingecko")
+dotenv.config({path: './config/config.env'})
+
+connectDB()
 const app = express();
 
-app.get('/', (req, res) => {
-});
+app.use(express.static(path.join(__dirname, "public")));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+coingecko.getCoin("the-graph");
+
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
+``;
