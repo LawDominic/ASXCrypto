2bfbed4a6f65f1a6e8a4b06708d04f6a8c691552
Dominic Law <dominic.law79@gmail.com>
2021-07-09 22:47:14 +1000

    Commenting

--- app.js
+++ app.js
@@ -5,30 +5,32 @@ const connectDB = require('./config/db')
 const exphbs = require('express-handlebars')
 const morgan = require('morgan')
 dotenv.config({path: './config/config.env'})
-
 const app = express();
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
-// APIs
+
+// @desc APIs
 const coingecko = require("./routes/api/coingecko")
 const asx = require("./routes/api/asx")
 
-// DB connection
+// @desc DB connection
 connectDB()
 
-// Handlebars
+// @desc Handlebars import and use .hbs extension
 app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', '.hbs')
 
-// Static
+// @desc Use static pages
 app.use(express.static(path.join(__dirname, 'public')))
 
-// Routes
+// @desc Routes
 app.use('/', require('./routes/index'))
 
 const PORT = process.env.PORT || 5000;
 
+// @desc API testing
 coingecko.getCoin("the-graph");
 asx.getShare("APT");
+
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
