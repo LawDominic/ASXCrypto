e079788a852ba5901763cb058d9234a278170b1a
Dominic Law <dominic.law79@gmail.com>
2021-07-09 22:48:30 +1000

    API methods + schema import

--- routes/api/asx.js
+++ routes/api/asx.js
@@ -1,12 +1,19 @@
 const axios = require('axios');
-
 const api = process.env.ASX_API;
+const Stock = require('../../models/Stock')
+
+// @desc Get coin from MongoDB
 function getShare(ticker) {
     axios.get(api + ticker)
         .then (function (response) {
             console.log(response.data)
             // return response.data
         });
+}
+
+// @desc Update MongoDB with updated data
+function updateStock() {
+    
 }
 
 module.exports = {
--- routes/api/coingecko.js
+++ routes/api/coingecko.js
@@ -1,23 +1,33 @@
 const axios = require('axios');
-
 const api = process.env.COINGECKO_API;
+const Coin = require('../../models/Coin.js')
 
+// @desc Get status of Coingecko API
 function getStatus() {
     axios.get(`${api}ping`)
     .then (function (response) {
-        console.log(response.data)
+        // console.log(response.data)
     })
     .catch (function (error) {
         console.log(error);
     });
 }
 
+// @desc Get coin from MongoDB
 function getCoin(id) {
     axios.get(`${api}coins/${id}`)
         .then (function (response) {
-            console.log(response.data.market_data.current_price.usd)
-            return response.data.market_data.current_price.usd;
-        });
+            // console.log(response.data)
+            return response.data;
+        }
+    )
+}
+
+// @desc Update MongoDB with updated data
+function updateCoin() {
+    // Get response data from Coingecko API
+
+    // Upload data to MongoDB
 }
 
 module.exports = {
