143b7befca5209575fd2079e98ab83065ae9c9e4
Dominic Law <dominic.law79@gmail.com>
2021-07-12 23:36:41 +1000

    Refactored

--- routes/api/asx.js
+++ routes/api/asx.js
@@ -7,57 +7,51 @@ const app = express();
 const api = process.env.ASX_API;
 const headerAPI = process.env.ASX_HEADER;
 let arrData = []
-let displayName
-// @desc Get coin from MongoDB
-function getShare(ticker) {
-    axios.get(api + ticker)
-        .then (function (response) {
-            arrData = response.data;
-            updateStock()
-        });
+let body = []
+
+// @desc Get stock from MongoDB
+function getStock() {
+    
 }
 
 // @desc Update MongoDB with updated data
-function updateStock(arrData) {
-    console.log(arrData)
-    axios.get(headerAPI + arrData.code + "/header")
-        .then (function (response) {
-            displayName = response.data.displayName
-        })
-    let body = {
-        ticker: arrData.code,
-        displayName,
-        lastPrice: arrData.last_price,
-        openPrice: arrData.open_price,
-        dailyHigh: arrData.day_high_price,
-        dailyLow: arrData.day_low_price,
-        dailyChange: arrData.change_price,
-        volume: arrData.volume,
-        previousDayPerDelta: arrData.previous_day_percentage_change,
-        yearHighPrice: arrData.year_high_price,
-        yearLowPrice: arrData.year_low_price,
-        pe: arrData.pe,
-        eps: arrData.eps,
-        divYield: arrData.annual_dividend_yield,
-        marketCap: arrData.market_cap,
-        shareNumber: arrData.number_of_sharess
-    }
-
-    app.post("/", async (request, response) => {
-        const stock = new Stock(body);
-      
-        try {
-          await stock.save();
-          response.send(stock);
-        } catch (error) {
-          response.status(500).send(error);
-        }
-      });
-
+async function updateStock(ticker) {
+    await axios
+        .get(api + ticker)
+            .then (response => {
+                arrData = response.data;
+                return axios.get(headerAPI + arrData.code + "/header")
+            })
+            .then (response => {
+                body = {
+                    ticker: arrData.code,
+                    displayName: response.data.data.displayName,
+                    lastPrice: arrData.last_price,
+                    openPrice: arrData.open_price,
+                    dailyHigh: arrData.day_high_price,
+                    dailyLow: arrData.day_low_price,
+                    dailyChange: arrData.change_price,
+                    volume: arrData.volume,
+                    previousDayPerDelta: arrData.previous_day_percentage_change,
+                    yearHighPrice: arrData.year_high_price,
+                    yearLowPrice: arrData.year_low_price,
+                    pe: arrData.pe,
+                    eps: arrData.eps,
+                    divYield: arrData.annual_dividend_yield,
+                    marketCap: arrData.market_cap,
+                    shareNumber: arrData.number_of_shares
+                }
+                console.log(body)
+            })
+            .catch(error => console.log(error.response))
 }
 
-
+function getBody() {
+    return body;
+}
 
 module.exports = {
-    getShare
+    getStock,
+    updateStock,
+    getBody
 }
