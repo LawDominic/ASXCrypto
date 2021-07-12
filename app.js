50dceb36765df30a577096607949b0fd69da0294
Dominic Law <dominic.law79@gmail.com>
2021-07-12 23:32:16 +1000

    Body parser +Stock API

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db')
 const exphbs = require('express-handlebars')
 const morgan = require('morgan')
+const bodyParser = require("body-parser");
 dotenv.config({path: './config/config.env'})
 const app = express();
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
+
+// @desc Middleware - bodyParser
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 // @desc APIs
 const coingecko = require("./routes/api/coingecko")
@@ -31,6 +36,20 @@ const PORT = process.env.PORT || 5000;
 
 // @desc API testing
 coingecko.getCoin("the-graph");
-asx.getShare("APT");
+
+// @desc Stock API testing
+const Stock = require("./models/Stock")
+app.get("/asx", async (request, response) => {
+    let body = asx.getBody();
+    console.log(body)
+    const stock = new Stock(body);
+    console.log("Starting POST")
+    try {
+      await stock.save();
+      response.send(stock);
+    } catch (error) {
+      response.status(500).send(error);
+    }
+  });
 
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
